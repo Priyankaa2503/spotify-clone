08957133d5ad6a8e92711be0831980036695d8a4
AdityaSurve <surveaditya521@gmail.com>
2023-06-29 10:52:20 +0530

    auth and user -  setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
