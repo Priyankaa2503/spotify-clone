c98a3bc72d03c8286a50bf7ce26bd7c5182eedf3
AdityaSurve <surveaditya521@gmail.com>
2023-06-30 18:18:54 +0530

    greetingCard music

--- client/pages/home/components/Banner/components/GreetingCard.tsx
+++ client/pages/home/components/Banner/components/GreetingCard.tsx
@@ -9,12 +9,12 @@ const GreetingCard: React.FC<GreetingCardProps> = ({ name, image }) => {
   const [showPlayButton, setShowPlayButton] = useState(false);
   return (
     <div
-      className="bg-[#303030] hover:bg-[#454545] transition duration-300 flex h-24 tracking-wider font-semibold cursor-pointer w-full rounded-md overflow-hidden"
+      className="bg-[#303030] hover:bg-[#454545] transition duration-300 flex h-20 tracking-wider font-semibold cursor-pointer w-full rounded-md overflow-hidden"
       onMouseEnter={() => setShowPlayButton(true)}
       onMouseLeave={() => setShowPlayButton(false)}
     >
       <Image src={image} alt="" className="h-full object-cover" />
-      <div className="h-full w-full px-5 flex items-center">{name}</div>
+      <div className="h-full w-[50%] px-5 flex items-center">{name}</div>
       <div className="w-24 h-full px-5 items-center flex">
         {showPlayButton && (
           <div className="bg-green-600 text-black h-14 w-14 rounded-full flex justify-center items-center">
