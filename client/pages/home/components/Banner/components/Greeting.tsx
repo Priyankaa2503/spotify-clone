a839e9a3247f503f7b3b66857312f991cdb00026
unknown <surveaditya521@gmail.com>
2023-07-11 17:56:54 +0530

    greet

--- client/pages/home/components/Banner/components/Greeting.tsx
+++ client/pages/home/components/Banner/components/Greeting.tsx
@@ -16,7 +16,7 @@ const Greeting = () => {
   return (
     <div className="w-full px-10 py-5 mt-5">
       <div className="text-5xl font-bold tracking-wide">{greeting}</div>
-      <div className="grid grid-cols-3 gap-x-5 gap-y-3 mt-5">
+      <div className="grid grid-cols-2 gap-x-3 gap-y-3 mt-5">
         {greetingData.map((data) => (
           <GreetingCard key={data.id} image={data.image} name={data.name} />
         ))}
--- client/pages/home/components/Banner/data/data.ts
+++ client/pages/home/components/Banner/data/data.ts
@@ -1,18 +1,14 @@
-import LinkedSongs from "@/public/images/Greetings/LikedSongs.png";
-import DanceElectronicMix from "@/public/images/Greetings/DanceElectronicMix.png";
 import MySongs from "@/public/images/Greetings/MySongs.png";
-import Netflix from "@/public/images/Greetings/NetflixPlaylist.png";
-import KDA from "@/public/images/Greetings/KDA.png";
 const greetingData = [
   {
     id: "linkedsongs",
     name: "Linked Songs",
-    image: LinkedSongs,
+    image: MySongs,
   },
   {
     id: "danceelectronicmix",
     name: "Dance/Electronic Mix",
-    image: DanceElectronicMix,
+    image: MySongs,
   },
   {
     id: "mysongs",
@@ -22,12 +18,17 @@ const greetingData = [
   {
     id: "netflix",
     name: "Netflix Playlist",
-    image: Netflix,
+    image: MySongs,
   },
   {
     id: "kda",
     name: "K/DA",
-    image: KDA,
+    image: MySongs,
+  },
+  {
+    id: "chillMix",
+    name: "Chill Mix",
+    image: MySongs,
   },
 ];
 export { greetingData };
