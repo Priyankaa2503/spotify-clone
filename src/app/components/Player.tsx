4d50e7de4ceb480b80f904643b590689476a3f59
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-08-06 21:47:10 +0530

    ui

---
+++ src/app/components/Player.tsx
@@ -0,0 +1,25 @@
+import { PauseCircleIcon, PlayCircleIcon } from '@heroicons/react/24/solid';
+import React, { useEffect, useState } from 'react';
+
+const Player = () => {
+   
+   
+
+    return (
+        <div className='h-24 bg-neutral-800 border-t border-neutral-700 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
+            <div className='flex items-center space-x-4'>
+              <img className='hidden md:inline h-10 w-10' src=""/>
+                <div>
+                    <p className='text-white text-sm'>Song Name</p>
+                    <p className='text-neutral-400 text-xs'>Artist Name</p>
+                </div>
+            </div>
+            <div className='flex items-center justify-center'>
+               <PauseCircleIcon  className='h-10 w-10' /> 
+            </div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Player;
