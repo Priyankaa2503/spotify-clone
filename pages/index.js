db5824c05c04ef4db1ee32d821a57f362eb4143d
Aditya <surveaditya521@gmail.com>
2023-08-08 12:30:44 +0530

    auth again

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Sidebar from "./components/Sidebar";
+import PlaylistView from "./components/PlaylistView";
+import Player from "./components/Player";
+export default function Home() {
+  return (
+    <div className="">
+      <main className="flex w-full h-screen overflow-hidden bg-black">
+        <Sidebar />
+        <PlaylistView />
+      </main>
+      <Player />
+    </div>
+  );
+}
